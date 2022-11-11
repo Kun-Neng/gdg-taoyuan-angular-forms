dcf4bc681f4eef386b3e645246fae5785dc219b6
Kun-Neng <kunneng.hung@gmail.com>
2022-11-11 22:02:57 +0800

    create forbidden validator for template driven form

---
+++ src/app/template-driven-form/forbidden-name.directive.ts
@@ -0,0 +1,21 @@
+import { Directive, Input } from '@angular/core';
+import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator } from '@angular/forms';
+import { forbiddenNameValidator } from '../reactive-form/forbidden-name.directive';
+
+@Directive({
+  selector: '[appForbiddenName]',
+  providers: [{
+    provide: NG_VALIDATORS,
+    useExisting: ForbiddenValidatorDirective,
+    multi: true
+  }]
+})
+export class ForbiddenValidatorDirective implements Validator {
+  @Input('appForbiddenName') forbiddenName = '';
+
+  validate(control: AbstractControl): ValidationErrors | null {
+    return this.forbiddenName ?
+      forbiddenNameValidator(new RegExp(this.forbiddenName, 'i'))(control) :
+      null;
+  }
+}
--- src/app/template-driven-form/template-driven-form.module.ts
+++ src/app/template-driven-form/template-driven-form.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { ForbiddenValidatorDirective } from './forbidden-validator.directive';
 import { TemplateDrivenFormRoutingModule } from './template-driven-form-routing.module';
 import { TemplateDrivenFormComponent } from './template-driven-form.component';
 
 @NgModule({
   imports: [CommonModule, FormsModule, TemplateDrivenFormRoutingModule],
-  declarations: [TemplateDrivenFormComponent]
+  declarations: [ForbiddenValidatorDirective, TemplateDrivenFormComponent]
 })
 export class TemplateDrivenFormModule { }
