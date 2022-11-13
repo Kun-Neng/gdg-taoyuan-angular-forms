3cd86d6152d8799a8b9102bf7aa66a5f113d987d
Kun-Neng <kunneng.hung@gmail.com>
2022-11-13 13:38:03 +0800

    add interface for the form

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -2,32 +2,37 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { forbiddenNameValidator } from './forbidden-name.directive';
 
+interface LoginForm {
+  account: FormControl<string>;
+  password: FormControl<string>;
+};
+
 @Component({
   selector: 'app-reactive-form',
   templateUrl: './reactive-form.component.html',
   styleUrls: ['./reactive-form.component.css']
 })
 export class ReactiveFormComponent implements OnInit {
-  // myForm = new FormGroup({
-  //   account: new FormControl('', [
-  //     Validators.required,
-  //     Validators.minLength(4),
-  //     forbiddenNameValidator(/admin/i)
-  //   ]),
-  //   password: new FormControl('', [
-  //     Validators.required,
-  //     Validators.minLength(6)
-  //   ])
-  // });
-
-  myForm = this.fb.group({
-    account: ['', {
-      validators: [Validators.required, Validators.minLength(4), forbiddenNameValidator(/admin/i)]
-    }],
-    password:['', {
-      validators: [Validators.required, Validators.minLength(6)]
-    }]
+  myForm = new FormGroup<LoginForm>({
+    account: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+      forbiddenNameValidator(/admin/i)
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6)
+    ])
   });
+
+  // myForm = this.fb.nonNullable.group({
+  //   account: ['', {
+  //     validators: [Validators.required, Validators.minLength(4), forbiddenNameValidator(/admin/i)]
+  //   }],
+  //   password:['', {
+  //     validators: [Validators.required, Validators.minLength(6)]
+  //   }]
+  // });
 
   get account() {
     return this.myForm.get('account');
